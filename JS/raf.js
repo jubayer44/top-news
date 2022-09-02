18490a1c091e736b8df7421254dda389c8695052
jubaer44 <jubayerahmedshamim44@gmail.com>
2022-09-03 03:02:42 +0600

    cards-sectin

--- JS/raf.js
+++ JS/raf.js
@@ -27,11 +27,33 @@ const displayNews = (allNewsCategories) => {
         // console.log(viewCategories.length + ' News Found In This Category')
         resultFound.innerText = viewCategories.length + ' News Found In This Category';
         resultsContainer.classList.remove('d-none')
-        return
+        
     }
 
+    const cardsContainer = document.getElementById('cards-container')
+    cardsContainer.textContent = '';
+    
     viewCategories.forEach(singleCategories => {
-        // console.log(singleCategories)
+        const {title, image_url, details, author} = singleCategories
+        // console.log(author.name)
+
+        const CardDiv = document.createElement('div')
+        CardDiv.classList.add('card', 'mb-3', 'my-3')
+        CardDiv.innerHTML = `
+        <div class="row g-0">
+                          <div class="col-md-4">
+                            <img src="..." class="img-fluid rounded-start" alt="...">
+                          </div>
+                          <div class="col-md-8">
+                            <div class="card-body">
+                              <h5 class="card-title">Card title</h5>
+                              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                          </div>
+                        </div>
+        `;
+        cardsContainer.appendChild(CardDiv)
 
 
     })

dfe5c0662db27eca662f906533ffacef0b82d58f
jubaer44 <jubayerahmedshamim44@gmail.com>
2022-09-03 21:24:24 +0600

    completed

--- JS/app.js
+++ JS/app.js
@@ -1,55 +1,46 @@
 const cardsContainer = document.getElementById('cards-container')
-
-
+const footerSection = document.getElementById('footer-section')
 
 const findCategories = (id) => {
+    footerSection.classList.remove('d-none')
     // spinner start
     loadingSpinner(true)
-    // console.log(id)
     fetch(`https://openapi.programming-hero.com/api/news/category/${id}`)
         .then(res => res.json())
         .then(data => displayNews(data))
-        // .catch(error => console.log(error))
 }
-
 
 const displayNews = (allNewsCategories) => {
 
     const viewCategories = allNewsCategories.data;
-    // console.log(viewCategories.length)
-    const resultsContainer = document.getElementById('results-container')
+    viewCategories.sort((x, y) => {
+        return y.total_view - x.total_view;
+    });
 
+    const resultsContainer = document.getElementById('results-container')
     const resultFound = document.getElementById('founded-results')
     resultFound.textContent = '';
 
     if (viewCategories.length === 0) {
-        // console.log('No News Found In This Category')
+        footerSection.classList.add('d-none')
         resultFound.innerText = 'News Not Found In This Category';
+        resultFound.classList.add('text-danger')
         resultsContainer.classList.remove('d-none')
         cardsContainer.textContent = '';
         //spinner stop
         loadingSpinner(false)
-        // const spinner = document.getElementById('spinner')
         return
     }
 
     else {
-        // console.log(viewCategories.length + ' News Found In This Category')
         resultFound.innerText = viewCategories.length + ' News Found In This Category';
         resultsContainer.classList.remove('d-none')
-
+        resultFound.classList.remove('text-danger')
     }
-
-    // const cardsContainer = document.getElementById('cards-container')
     cardsContainer.textContent = '';
 
-    // let arrayNum = []
-    // let arrayElement = []
     viewCategories.forEach(singleCategories => {
-        const { title, image_url, thumbnail_url, details, author, total_view, rating } = singleCategories
-        // arrayNum.push(total_view, singleCategories)  
-        // arrayElement.push(total_view)
-        // console.log(singleCategories.details)
+        const { title, image_url, thumbnail_url, details, author, total_view, rating } = singleCategories;
 
         const CardDiv = document.createElement('div')
         CardDiv.classList.add('card', 'mb-3', 'my-3')
@@ -61,22 +52,23 @@ const displayNews = (allNewsCategories) => {
                 <div class="col-md-9">
                     <div class="card-body">
                         <h3 class="card-title">${title}</h2>
-                        <p class="card-text">${details.length > 500 ? details.slice(0, 500) + '...' : details}</p>
+                        <p class="card-text text-muted">${details.length > 500 ? details.slice(0, 500) + '...' : details}</p>
                         
                         <div class="d-md-flex justify-content-between" style="margin-top: 15%">
                             <div class="d-flex gap-2">
-                                <img class="rounded-circle" src="${author.img}" alt="" style="width: 50px; height: 50px;">
+                                <img class="rounded-circle" src="${author ? author.img : 'N/A'}" alt="" style="width: 50px; height: 50px;">
                                 <div>
                                     <h5>${author.name ? author.name : 'N/A'}</h5>
-                                    <p>${author.published_date ? author.published_date : 'N/A'}</p>
+                                    <p class=" text-muted">${author.published_date ? author.published_date : 'N/A'}</p>
                                 </div>
                             </div>
-                            <p><i class="fa-regular fa-eye me-2"></i>${total_view}</p>
-                            <p>Rating: ${rating.number}</p>
-                            <button onclick="newsModal('${image_url}', '${title}', '${details.slice(0, 95) + '...'}')" class="btn btn-primary h-25" data-bs-toggle="modal" data-bs-target="#newsModal">View</button>
+                            <p class=" text-muted"><i class="fa-regular fa-eye me-2"></i>${total_view ? total_view : 'N/A'}</p>
+                            <p class=" text-muted">Rating: ${rating ? rating.number : 'N/A'}</p>
+                            <button onclick="openModal('${title}', '${image_url}', '${total_view ? total_view : 'N/A'}', '${author.img}', '${author.name ? author.name : 'N/A'}', '${author.published_date ? author.published_date : 'N/A'}')" type="button" class="btn btn-primary h-25" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                            Details
+                          </button>
                         </div>
-
-                    </>
+                    </div>
                 </div>
             </div>
         `;
@@ -85,34 +77,28 @@ const displayNews = (allNewsCategories) => {
         //spinner stop
         loadingSpinner(false)
     })
-    
-
-
-    // console.log(arrayNum.sort((a, b) => b-a), arrayElement)
-    // function findBySort(element) {
-
-        // console.log(arrayNum.sort((a, b) => b-a))
-       
-    // }
 
 }
-// findCategories()
 
-   // Modal 
+// Modal 
+const modalTitle = document.getElementById('exampleModalLabel')
+const modalBody = document.getElementById('modals-body')
+const openModal = (title, img, view, authImg, name, date) => {
 
-   const modalTitle = document.getElementById('newsModalLabel')
-   const modalBody = document.getElementById('modal-body')
-   const newsModal = (img, title, details) => {
-
-    //    console.log(details)
-       modalTitle.innerText = `${title}`;
-       modalBody.innerHTML = `
-       <img src="${img}" class="w-100 rounded-start" alt="...">
-       
-       `;
-
-   }
-
+    modalTitle.innerText = `${title}`;
+    modalBody.innerHTML = `
+    <img src="${img}" class="w-100 rounded-start" alt="...">
+    
+    <div class="d-flex gap-2 mt-3">
+         <img class="rounded-circle" src="${authImg}" alt="" style="width: 50px; height: 50px;">
+     <div>
+         <h5>${name}</h5>
+         <p class=" text-muted">${date ? date : 'N/A'}</p>
+    </div>
+    <p class=" text-muted">View: ${view}</p>
+    </div>
+    `;
+}
 
 const loadCategory = () => {
     fetch('https://openapi.programming-hero.com/api/news/categories')
@@ -121,26 +107,19 @@ const loadCategory = () => {
         .catch(error => console.log(error))
 }
 
-
 //Display Categories
 const displayCategory = (categories) => {
-    // console.log(categories)
     const categoriesContainer = document.getElementById('categories-container')
 
     categories.forEach(category => {
-        // console.log(category.category_id)
-
         const categoryText = document.createElement('div')
-        categoryText.innerHTML = `<h6 onclick="findCategories('${category.category_id}')" style="cursor: pointer;">${category.category_name}</h6>`;
+        categoryText.innerHTML = `<h6 onclick="findCategories('${category.category_id}')" style="cursor: pointer;" class="hover-btn fw-bold">${category.category_name}</h6>`;
 
         categoriesContainer.appendChild(categoryText)
     });
 }
 
- 
-
-
-//spinner function
+//spinner 
 const spinner = document.getElementById('spinner')
 const loadingSpinner = (isLoading) => {
     if (isLoading) {
@@ -149,9 +128,7 @@ const loadingSpinner = (isLoading) => {
     else {
         spinner.classList.add('d-none')
     }
-}
+};
 
 loadCategory()
-
-
 
